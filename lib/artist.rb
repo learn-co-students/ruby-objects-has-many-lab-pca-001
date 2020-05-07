c068c01e6b5f8c241711fa71245538eead13ada0
Pat McClernan <patmccler@gmail.com>
2020-05-07 13:44:47 -0400

    builds 4 classes out

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+require "song.rb"
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    Song.new(song_name).artist = self
+  end
+
+  def self.song_count
+    Song.all.count(&:artist)
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self}
+  end
+end
